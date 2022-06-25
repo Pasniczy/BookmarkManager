55f1592bbd293b329391d20dfeb3d5d1db5091bb
Pasniczy <bartoszpasnik@gmail.com>
2022-06-25 20:10:28 +0200

    Setup ESLint and Prettier in frontend project

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,45 @@
 module.exports = {
   root: true,
-  extends: ["react-app", "react-app/jest"],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: ['plugin:prettier/recommended', 'airbnb', 'airbnb/hooks', 'plugin:react/jsx-runtime'],
+  plugins: ['prettier', 'unused-imports'],
+  rules: {
+    'prettier/prettier': 2,
+    semi: 2,
+    'no-undef': 0,
+    'no-shadow': 0,
+    'arrow-body-style': 0,
+    'no-nested-ternary': 2,
+    'no-trailing-spaces': 2,
+    'unused-imports/no-unused-imports': 1,
+    'react/function-component-definition': 0,
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'airbnb-typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'airbnb-typescript/base',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      rules: {
+        '@typescript-eslint/no-floating-promises': 0,
+        '@typescript-eslint/unbound-method': 0,
+        '@typescript-eslint/no-unused-vars': 1,
+        '@typescript-eslint/no-empty-interface': 1,
+        '@typescript-eslint/no-use-before-define': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/comma-dangle': 0,
+      },
+    },
+  ],
 };
--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders App", () => {
+test('renders App', () => {
   render(<App />);
   const text = screen.getByText(/App/i);
   expect(text).toBeInTheDocument();
