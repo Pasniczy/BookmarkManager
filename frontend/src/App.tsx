5a019b6788c932b60aaae1d74e2d977dd257e2e3
Pasniczy <bartoszpasnik@gmail.com>
2022-06-29 22:56:34 +0200

    Improve routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,19 +6,21 @@ import { BookmarkView } from './views/Bookmark/BookmarkView';
 import { AddBookmarkView } from './views/AddBookmark/AddBookmarkView';
 import { EditBookmarkView } from './views/EditBookmark/EditBookmarkView';
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/bookmarks" element={<BookmarksView />} />
-        <Route path="/bookmarks/add" element={<AddBookmarkView />} />
-        <Route path="/bookmarks/edit/:id" element={<EditBookmarkView />} />
-        <Route path="/bookmarks/:id" element={<BookmarkView />} />
-        <Route path="/" element={<HomeView />} />
+        <Route path="bookmarks">
+          <Route index element={<BookmarksView />} />
+          <Route path="add" element={<AddBookmarkView />} />
+          <Route path="edit/:id" element={<EditBookmarkView />} />
+          <Route path=":id" element={<BookmarkView />} />
+        </Route>
+        <Route index element={<HomeView />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
