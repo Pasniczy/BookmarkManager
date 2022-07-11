19cada7a1971034453fdf9c1640a614971f9ccb8
Bartosz Pasnik <bartoszpasnik@gmail.com>
2022-07-12 00:01:41 +0200

    Add handleMySQLError middleware

--- backend/utils/errors.ts
+++ backend/utils/errors.ts
@@ -1,20 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
+import { QueryError } from 'mysql2';
 
 export class ValidationError extends Error {}
 
 export class AuthError extends Error {}
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const handleMySQLError = (err: QueryError, req: Request, res: Response, next: NextFunction) => {
   console.log(err);
 
-  if (err instanceof ValidationError) {
-    return res.status(400).json({ message: err.message });
+  switch (err.code) {
+    case 'ER_DUP_ENTRY':
+      return res.status(400).json({
+        message: 'User with given email already exists',
+      });
+    default:
+      next(err);
   }
+};
 
-  if (err instanceof AuthError) {
-    return res.status(401).json({ message: err.message || 'User unauthorized' });
-  }
+export const handleError = (err: Error, req: Request, res: Response) => {
+  console.log(err);
+
+  if (err instanceof ValidationError) return res.status(400).json({ message: err.message });
+
+  if (err instanceof AuthError) return res.status(401).json({ message: err.message || 'User unauthorized' });
 
   res.status(500).json({ message: 'Internal Server Error' });
 };
